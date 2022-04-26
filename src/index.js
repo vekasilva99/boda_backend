fae9d45dd5b69e5f66d6996df07f268e7722630f
Valeska Silva <valeska.silva@wuhk.app>
2022-04-25 20:39:13 -0400

    Try

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+// const exphbs = require('express-handlebars');
 require("dotenv").config();
 
 const port = process.env.PORT || 9000;
 const app = express();
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars'); 
+
+
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 const invitadosRouter = require('./routes/invitados');
 const invitacionRouter = require('./routes/invitacion');
 
 app.use('/invitados', invitadosRouter);
 app.use('/invitaciones', invitacionRouter);
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true }
-).then(()=>{console.log("Conectado Mongo")}).catch((error)=>{console.log("error",error)});
-
 
 app.get('/', (req, res) => {
     res.send('Boda API')
